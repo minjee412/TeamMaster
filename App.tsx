ea023c289e003a1b2900fbc95a1a8dcc1dd779a0
minjee412 <minjee.412@kakao.com>
2021-11-03 13:12:35 +0900

    update

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-// In App.js in a new project  ddddd dddddgitd
+// In App.js in a new project
 import * as React from 'react';
 import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';

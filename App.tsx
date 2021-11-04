c889bf9654d55f667ae0594d30a574fe3c51accc
minjee412 <minjee.412@kakao.com>
2021-11-04 11:54:17 +0900

    2021.11.04 navigation실습

--- App.tsx
+++ App.tsx
@@ -16,10 +16,14 @@ function HomeScreen({ navigation }) {
   );
 }
 
-function DetailsScreen() {
+function DetailsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
